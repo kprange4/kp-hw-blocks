dbb0b4a4ddc7787992a9677898de3bc53928782e
Kaylee Prange <kprange4@my.wctc.edu>
2025-02-23 15:01:59 -0600

    create plugin

---
+++ build/kp-hw-blocks/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '2bc47c8e39334fc23e9e');
---
+++ build/kp-hw-blocks/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '3d6f62c9e4289fe58b82');
