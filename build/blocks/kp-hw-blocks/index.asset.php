aedc95d55cc1f854175cbe1445e1cd6ace8e2af0
Kaylee Prange <kprange4@my.wctc.edu>
2025-02-23 23:05:12 -0600

    fixed styles

--- build/blocks/kp-hw-blocks/index.asset.php
+++ build/blocks/kp-hw-blocks/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '4f741dae14f2f5050bf8');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '489f7d02a23fb72ed7cb');
